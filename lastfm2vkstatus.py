ba3676535d9556832da17feed80dc6d4d7dcb133
DiSonDS <dison.ds@gmail.com>
2018-10-15 17:26:35 +0300

    Initial commit

---
+++ lastfm2vkstatus.py
@@ -0,0 +1,46 @@
+import pylast
+import vk_api
+import time
+import sys
+from settings import *
+
+
+def main():
+
+    # Lastfm init
+    network = pylast.LastFMNetwork(api_key=API_KEY, api_secret=API_SECRET,
+                                   username=USERNAME, password_hash=pylast.md5(PASSWORD))
+
+    lastfm = pylast.User("", network)
+
+    # VK init
+    vk_session = vk_api.VkApi(token=VK_USER_TOKEN)
+    vk = vk_session.get_api()
+
+    last_track = ''
+
+    while True:
+        if lastfm:
+            results = lastfm.get_now_playing()
+            if not results:
+                if last_track:
+                    last_track = ''
+                    vk.status.set(text="")
+                time.sleep(INTERVAL)
+                continue
+            if results != last_track:
+                time_now = time.strftime("%H:%M", time.localtime())
+                status_text = f"🎧 Last.fm | {results}"
+                print(f"[{time_now}] {status_text}")
+                vk.status.set(text=status_text)
+                last_track = results
+                time.sleep(INTERVAL)
+            else:
+                time.sleep(INTERVAL)
+        else:
+            print(f"Can't get user")
+            sys.exit()
+
+
+if __name__ == "__main__":
+    main()
